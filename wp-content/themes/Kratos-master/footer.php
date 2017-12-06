21c8e3ced61b09d3c6aea458f5e0a08dd60a7f72
yangliuchan <287643563@qq.com>
2017-12-06 14:54:07 +0800

    第一次保存

---
+++ wp-content/themes/Kratos-master/footer.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * @package Vtrois
+ * @version 2.4
+ */
+?>
+				<footer>
+					<div id="footer">
+						<div class="cd-tool visible-lg text-center">
+							<?php if ( kratos_option( 'cd_gb' ) && kratos_option( 'cd_weixin' ) ) { ?>
+						   		<a rel="nofollow" class="cd-gb-a" href="<?php echo kratos_option('guestbook_links'); ?>"><span class="fa fa-book"></span></a>	
+						   	<?php } elseif( kratos_option( 'cd_gb' ) && !kratos_option( 'cd_weixin' ) ){ ?>
+						   		<a rel="nofollow" class="cd-gb-b" href="<?php echo kratos_option('guestbook_links'); ?>"><span class="fa fa-book"></span></a>	
+						   	<?php } ?>
+						   	<?php if ( kratos_option( 'cd_weixin' ) ) : ?>
+						   		<a id="weixin-img" class="cd-weixin"><span class="fa fa-weixin"></span><div id="weixin-pic"><img src="<?php echo kratos_option('weixin_image') ?>"></div></a>
+						   	<?php endif; ?>
+						    <a class="cd-top cd-is-visible cd-fade-out"><span class="fa fa-chevron-up"></span></a>
+						</div>
+						<div class="container">
+							<div class="row">
+								<div class="col-md-6 col-md-offset-3 footer-list text-center">
+									<p class="kratos-social-icons">
+									<?php echo (!kratos_option('social_weibo')) ? '' : '<a target="_blank" rel="nofollow" href="' . kratos_option('social_weibo') . '"><i class="fa fa-weibo"></i></a>'; ?>
+									<?php echo (!kratos_option('social_tweibo')) ? '' : '<a target="_blank" rel="nofollow" href="' . kratos_option('social_tweibo') . '"><i class="fa fa-tencent-weibo"></i></a>'; ?>
+									<?php echo (!kratos_option('social_twitter')) ? '' : '<a target="_blank" rel="nofollow" href="' . kratos_option('social_twitter') . '"><i class="fa fa-twitter"></i></a>'; ?>
+									<?php echo (!kratos_option('social_facebook')) ? '' : '<a target="_blank" rel="nofollow" href="' . kratos_option('social_facebook') . '"><i class="fa fa-facebook-official"></i></a>'; ?>
+									<?php echo (!kratos_option('social_linkedin')) ? '' : '<a target="_blank" rel="nofollow" href="' . kratos_option('social_linkedin') . '"><i class="fa fa-linkedin-square"></i></a>'; ?>
+									<?php echo (!kratos_option('social_github')) ? '' : '<a target="_blank" rel="nofollow" href="' . kratos_option('social_github') . '"><i class="fa fa-github"></i></a>'; ?>
+									</p>
+									<p>Copyright <?php echo date('Y'); ?> <a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a>. All Rights Reserved.<br>Theme <a href="https://github.com/vtrois/kratos" target="_blank" rel="nofollow">Kratos</a> made by <a href="https://www.vtrois.com/" target="_blank" rel="nofollow">Vtrois</a>
+									<?php if(kratos_option('icp_num')){?><br><a href="http://www.miitbeian.gov.cn/" rel="external nofollow" target="_blank"><?php echo kratos_option( 'icp_num' ); } ?></a><?php if(kratos_option('gov_num')){?><br><a href="<?php echo kratos_option( 'gov_link' ); ?>" rel="external nofollow" target="_blank"><i class="govimg"></i><?php echo kratos_option( 'gov_num' ); ?></a><?php }?></p><p><?php echo (!kratos_option('site_tongji')) ? '' : kratos_option('site_tongji'); ?></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</footer>
+			</div>
+		</div>
+		<?php wp_footer();?>
+		<?php if ( kratos_option('site_sa') ) : ?>
+		<script type="text/javascript">
+			if ($("#main").height() > $("#sidebar").height()) {
+				var footerHeight = 0;
+				if ($('#page-footer').length > 0) {
+					footerHeight = $('#page-footer').outerHeight(true);
+				}
+				$('#sidebar').affix({
+					offset: {
+						top: $('#sidebar').offset().top - 30,
+						bottom: $('#footer').outerHeight(true) + footerHeight + 6
+					}
+				});
+			}
+		</script>
+		<?php endif; ?>
+	</body>
+</html>
---
+++ wp-content/themes/Kratos-master/single.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package Vtrois
+ * @version 2.3
+ */
+
+$sidebar = kratos_option('side_bar');
+$sidebar = (empty($sidebar)) ? 'right_side' : $sidebar;
+get_header();
+get_header('banner'); ?>
+<div id="kratos-blog-post" style="background:<?php echo kratos_option('background_index_color'); ?>">
+	<div class="container">
+		<div class="row">
+			<?php if($sidebar == 'left_side'){ ?>
+			<aside id="kratos-widget-area" class="col-md-4 hidden-xs hidden-sm scrollspy">
+                <div id="sidebar">
+                    <?php dynamic_sidebar('sidebar_tool'); ?>
+                </div>
+            </aside>
+			<?php } ?>
+            <section id="main" class='<?php echo ($sidebar == 'single') ? 'col-md-12' : 'col-md-8'; ?>'>
+				<?php if (have_posts()) : the_post(); update_post_caches($posts); ?>
+				<article>
+					<div class="kratos-hentry kratos-post-inner clearfix">
+						<header class="kratos-entry-header">
+							<h1 class="kratos-entry-title text-center"><?php the_title(); ?></h1>
+							<div class="kratos-post-meta text-center">
+								<span>
+								<i class="fa fa-calendar"></i> <?php echo get_the_date(); ?>
+				                <i class="fa fa-commenting-o"></i> <?php comments_number('0', '1', '%'); ?>条评论
+				                <i class="fa fa-eye"></i> <?php echo kratos_get_post_views();?>次阅读
+				                <i class="fa fa-thumbs-o-up"></i> <?php if( get_post_meta($post->ID,'love',true) ){ echo get_post_meta($post->ID,'love',true); } else { echo '0'; }?>人点赞
+								</span>
+							</div>
+						</header>
+						<div class="kratos-post-content">
+						<?php if ( kratos_option('ad_show_1') ): ?>
+							<a href="<?php echo kratos_option('ad_link_1'); ?>"><img src="<?php echo kratos_option('ad_img_1')?>"></a>
+	                    <?php endif ?>
+                        <?php the_content(); ?>
+						<?php if ( kratos_option('ad_show_2') ): ?>
+							<a href="<?php echo kratos_option('ad_link_2'); ?>"><img src="<?php echo kratos_option('ad_img_2')?>"></a>
+	                    <?php endif ?>
+						</div>
+						<footer class="kratos-entry-footer clearfix">
+							<div class="post-like-donate text-center clearfix" id="post-like-donate">
+							<?php if ( kratos_option( 'post_like_donate' ) ) : ?>
+				   			<a href="<?php echo kratos_option('donate_links'); ?>" class="Donate"><i class="fa fa-bitcoin"></i> 打赏</a>
+				   			<?php endif; ?>
+				   			<a href="javascript:;" id="btn" data-action="love" data-id="<?php the_ID(); ?>" class="Love <?php if(isset($_COOKIE['love_'.$post->ID])) echo 'done';?>" ><i class="fa fa-thumbs-o-up"></i> 点赞</a>
+							<?php if ( kratos_option( 'post_share' ) ) : ?>
+							<a href="javascript:;"  class="Share" ><i class="fa fa-share-alt"></i> 分享</a>
+								<?php require_once( get_template_directory() . '/inc/share.php'); ?>
+							<?php endif; ?>
+				    		</div>
+							<div class="footer-tag clearfix">
+								<div class="pull-left">
+								<i class="fa fa-tags"></i>
+								<?php if ( get_the_tags() ) { the_tags('', ' ', ''); } else{ echo '<a>No Tag</a>';  }?>
+								</div>
+							</div>
+						</footer>
+					</div>
+					<?php if ( kratos_option( 'post_cc' )==1 ) : ?>
+					<div class="kratos-hentry kratos-copyright text-center clearfix">
+						<img alt="知识共享许可协议" src="<?php echo get_template_directory_uri(); ?>/images/licenses.png">
+						<h5>本作品采用 <a rel="license nofollow" target="_blank" href="http://creativecommons.org/licenses/by-sa/4.0/">知识共享署名-相同方式共享 4.0 国际许可协议</a> 进行许可</h5>
+					</div>
+					<?php endif; ?>
+					<nav class="navigation post-navigation clearfix" role="navigation">
+						<?php
+						$prev_post = get_previous_post(TRUE);
+						if(!empty($prev_post)):?>
+						<div class="nav-previous clearfix">
+							<a title="<?php echo $prev_post->post_title;?>" href="<?php echo get_permalink($prev_post->ID);?>">&lt; 上一篇</a>
+						</div>
+						<?php endif;?>
+						<?php
+						$next_post = get_next_post(TRUE);
+						if(!empty($next_post)):?>
+						<div class="nav-next">
+							<a title="<?php echo $next_post->post_title;?>" href="<?php echo get_permalink($next_post->ID);?>">下一篇 &gt;</a>
+						</div>
+						<?php endif;?>
+					</nav>
+					<?php comments_template(); ?>
+				</article>
+				<?php endif; ?>
+			</section>
+			<?php if($sidebar == 'right_side'){ ?>
+				<aside id="kratos-widget-area" class="col-md-4 hidden-xs hidden-sm scrollspy">
+	                <div id="sidebar">
+	                    <?php dynamic_sidebar('sidebar_tool'); ?>
+	                </div>
+	            </aside>
+			<?php } ?>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
