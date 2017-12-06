0a18f9934abef47cf090f95869170c7583ae37fd
yangliuchan <287643563@qq.com>
2017-12-06 22:56:23 +0800

    H 2017-12-06 22:56

--- wp-content/themes/bl/functions.php
+++ wp-content/themes/bl/functions.php
@@ -7,7 +7,7 @@
  */
 
 function favicon_link() {
-    echo '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />' . "n";
+    echo '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />';
 }
 add_action('wp_head', 'favicon_link');
 
